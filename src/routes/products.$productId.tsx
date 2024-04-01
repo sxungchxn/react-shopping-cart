90b8b9bae3c766daa54c410fc7d250c039464a64
Yangseungchan <uvzone@naver.com>
2024-04-01 21:50:47 +0900

    chore: 제품 상세 반응형 적용

--- src/routes/products.$productId.tsx
+++ src/routes/products.$productId.tsx
@@ -1,6 +1,6 @@
 import { productDetailOption } from '@/queries'
 import { Image, SquareButton } from '@/components'
-import { flex, vstack } from '@styled-system/patterns'
+import { flex } from '@styled-system/patterns'
 import { useSuspenseQuery } from '@tanstack/react-query'
 import { createFileRoute } from '@tanstack/react-router'
 import { Suspense } from 'react'
@@ -21,43 +21,49 @@ function ProductDetail() {
   } = useSuspenseQuery(productDetailOption(Number(productId)))
 
   return (
-    <div
-      className={vstack({
-        width: '640px',
-        gap: '0px',
-      })}
-    >
-      <Image
-        size="lg"
-        src={imageUrl}
-        alt={name}
-        className={css({
-          marginBottom: '20px',
-        })}
-      />
-      <h2 className={css({ textStyle: 'heading1', marginBottom: '20px' })}>{name}</h2>
-      <div
-        className={css({
-          width: '100%',
-          height: '2px',
-          backgroundColor: 'brown',
-          marginBottom: '30px',
-        })}
-      />
-      <div
-        className={flex({
-          width: '100%',
-          justifyContent: 'space-between',
-          paddingX: '35px',
-          marginBottom: '60px',
-        })}
-      >
-        <span className={css({ textStyle: 'body1' })}>금액</span>
-        <span className={css({ textStyle: 'body1' })}>{price}원</span>
+    <div className={container}>
+      <Image size="lg" src={imageUrl} alt={name} />
+      <div className={css({ width: '100%' })}>
+        <div className={productName}>
+          <h2
+            className={css({
+              textStyle: 'heading1',
+              marginBottom: '20px',
+              paddingX: '36px',
+              paddingBottom: '24px',
+              borderBottom: '2px solid token(colors.brown)',
+            })}
+          >
+            {name}
+          </h2>
+        </div>
+        <div className={productPrice}>
+          <span className={css({ textStyle: 'title1' })}>금액</span>
+          <span className={css({ textStyle: 'title1' })}>{price}원</span>
+        </div>
+        <SquareButton color="secondary">
+          <span className={css({ textStyle: 'heading2' })}>장바구니</span>
+        </SquareButton>
       </div>
-      <SquareButton color="secondary">
-        <span className={css({ textStyle: 'heading2' })}>장바구니</span>
-      </SquareButton>
     </div>
   )
 }
+
+const container = css({
+  display: 'flex',
+  width: '100%',
+  maxWidth: '1280px',
+  paddingX: '32px',
+  alignItems: { base: 'center', xl: 'flex-start' },
+  flexDir: { base: 'column', xl: 'row' },
+  gap: { base: '32px', xl: '100px' },
+})
+
+const productName = css({ display: 'flex', flexDirection: 'column', width: '100%' })
+
+const productPrice = flex({
+  width: '100%',
+  justifyContent: 'space-between',
+  paddingX: '35px',
+  marginBottom: '60px',
+})
